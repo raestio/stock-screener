ceb32258f6f3ba8c1d5195d05066da2648d6c7f2
Rastislav Zlacký <rasto.zlacky@gmail.com>
2019-01-27 16:17:56 +0100

    Add tests

--- backend/src/test/java/com/rasto/stockscreener/StockScreenerApplicationTests.java
+++ backend/src/test/java/com/rasto/stockscreener/StockScreenerApplicationTests.java
@@ -1,7 +1,9 @@
 package com.rasto.stockscreener;
 
+import com.rasto.stockscreener.dto.StockDataDTO;
 import com.rasto.stockscreener.dto.StockSearchDTO;
 import com.rasto.stockscreener.service.StockSearchService;
+import com.rasto.stockscreener.service.StockTimeSeriesService;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDate;
 import java.util.Currency;
 import java.util.List;
 
@@ -18,6 +21,9 @@ public class StockScreenerApplicationTests {
 
 	@Autowired
 	private StockSearchService stockSearchService;
+
+	@Autowired
+	private StockTimeSeriesService stockTimeSeriesService;
 
 	@Test
 	public void contextLoads() {
@@ -38,6 +44,42 @@ public class StockScreenerApplicationTests {
 	public void testStockSearchByKeywordThatDoesNotExist() {
 		List<StockSearchDTO> stockSearchDataList = stockSearchService.searchStockByKeyword("IUSGafewfeferfERGREGafwefaewfe");
 		Assert.assertTrue(stockSearchDataList.isEmpty());
+	}
+
+	@Test
+	public void testMonthlyTimeSeries() {
+		List<StockDataDTO> stockDataDTOS = stockTimeSeriesService.getMonthly("AAPL");
+		Assert.assertTrue(stockDataDTOS.size() > 150);
+		StockDataDTO stockDataDTO = stockDataDTOS.stream().filter(stockData -> stockData.getDateTime().equals(LocalDate.parse("2018-11-30").atStartOfDay())).findFirst().get();
+		Assert.assertEquals(stockDataDTO.getClose(), Double.valueOf(178.58));
+		Assert.assertEquals(stockDataDTO.getOpen(), Double.valueOf(219.05));
+		Assert.assertEquals(stockDataDTO.getLow(), Double.valueOf(170.26));
+		Assert.assertEquals(stockDataDTO.getHigh(), Double.valueOf(222.36));
+		Assert.assertEquals(stockDataDTO.getVolume(), Long.valueOf(961321947));
+	}
+
+	@Test
+	public void testWeeklyTimeSeries() {
+		List<StockDataDTO> stockDataDTOS = stockTimeSeriesService.getWeekly("AAPL");
+		Assert.assertTrue(stockDataDTOS.size() > 150);
+		StockDataDTO stockDataDTO = stockDataDTOS.stream().filter(stockData -> stockData.getDateTime().equals(LocalDate.parse("2019-01-11").atStartOfDay())).findFirst().get();
+		Assert.assertEquals(stockDataDTO.getClose(), Double.valueOf(152.2900));
+		Assert.assertEquals(stockDataDTO.getOpen(), Double.valueOf(148.7));
+		Assert.assertEquals(stockDataDTO.getLow(), Double.valueOf(145.9000));
+		Assert.assertEquals(stockDataDTO.getHigh(), Double.valueOf(154.5300));
+		Assert.assertEquals(stockDataDTO.getVolume(), Long.valueOf(203706070));
+	}
+
+	@Test
+	public void testDailyTimeSeries() {
+		List<StockDataDTO> stockDataDTOS = stockTimeSeriesService.getDaily("AAPL");
+		Assert.assertTrue(stockDataDTOS.size() >= 100);
+		StockDataDTO stockDataDTO = stockDataDTOS.stream().filter(stockData -> stockData.getDateTime().equals(LocalDate.parse("2018-09-12").atStartOfDay())).findFirst().get();
+		Assert.assertEquals(stockDataDTO.getClose(), Double.valueOf(221.0700));
+		Assert.assertEquals(stockDataDTO.getOpen(), Double.valueOf(224.9400));
+		Assert.assertEquals(stockDataDTO.getLow(), Double.valueOf(219.8400));
+		Assert.assertEquals(stockDataDTO.getHigh(), Double.valueOf(225.0000));
+		Assert.assertEquals(stockDataDTO.getVolume(), Long.valueOf(49278740));
 	}
 }
 
