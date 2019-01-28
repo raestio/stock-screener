667da187b9c667a837623912d3a9f45607ea1c06
Rastislav Zlacký <rasto.zlacky@gmail.com>
2019-01-28 23:33:17 +0100

    Add more functionalities to client

--- client/richclient/src/main/java/com/rasto/richclient/MainController.java
+++ client/richclient/src/main/java/com/rasto/richclient/MainController.java
@@ -1,17 +1,24 @@
 package com.rasto.richclient;
 
 import com.rasto.stockscreener.model.Stock;
+import com.rasto.stockscreener.model.StockData;
 import com.rasto.stockscreener.model.StockSearch;
 import com.rasto.stockscreener.restclient.AuthException;
 import com.rasto.stockscreener.restclient.StockScreenerRestClient;
 import com.rasto.stockscreener.restclient.StockScreenerRestClientImpl;
+import com.rasto.stockscreener.restclient.TimeSeriesType;
 import com.rasto.stockscreener.restclient.UserNotLoggedInException;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
 
 import java.util.ArrayList;
@@ -33,11 +40,39 @@ public class MainController {
     public Button clearButton;
 
     @FXML
+    public Button daily;
+
+    @FXML
+    public Button weekly;
+
+    @FXML
+    public Button monthly;
+
+    @FXML
+    public Button favourite;
+    public static final String ADD_TO_FAVOURITES = "Add to favourites";
+    public static final String REMOVE_FROM_FAVOURITES = "Remove from favourites";
+
+    @FXML
     public ListView<Stock> stocksListView;
+
+    private Stock stockCurrentlyInChart;
+
+    @FXML
+    public CategoryAxis xAxis;
+
+    @FXML
+    public NumberAxis yAxis;
+
+    @FXML
+    public LineChart<String, Number> chart;
+    public static final TimeSeriesType DEFAULT_TIME_SERIES_TYPE = TimeSeriesType.DAILY;
 
     private StockScreenerRestClient stockScreenerRestClient;
 
     public void initialize() {
+        favourite.setDisable(true);
+        setDisableChartButtons(true);
         stockScreenerRestClient = new StockScreenerRestClientImpl();
         try {
             stockScreenerRestClient.login("test", "test");
@@ -47,13 +82,51 @@ public class MainController {
         initializeSearchButton();
         initializeClearButton();
         initializeStocksListView();
+        initializeFavouriteButton();
+    }
+
+    private void initializeFavouriteButton() {
+        favourite.setOnMouseClicked(event -> {
+            try {
+                if (favourite.getText().equals(ADD_TO_FAVOURITES)) {
+                    stockScreenerRestClient.addToFavourites(stockCurrentlyInChart);
+                } else if (favourite.getText().equals(REMOVE_FROM_FAVOURITES)) {
+                    stockScreenerRestClient.removeFromFavourites(stockCurrentlyInChart.getSymbol());
+                }
+            } catch (UserNotLoggedInException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private void setDisableChartButtons(boolean b) {
+        weekly.setDisable(b);
+        monthly.setDisable(b);
+        daily.setDisable(b);
     }
 
     private void initializeClearButton() {
-        clearButton.setOnMouseClicked(event -> stocksListView.getItems().clear());
+        clearButton.setOnMouseClicked(event -> {
+            fillListWithFavourites();
+        });
+    }
+
+    private void clearStocksList() {
+        stocksListView.getItems().clear();
+    }
+
+    private void fillListWithFavourites() {
+        clearStocksList();
+        try {
+            var favs = stockScreenerRestClient.getFavourites();
+            fillListViewWithStocks(favs);
+        } catch (UserNotLoggedInException e) {
+            e.printStackTrace();
+        }
     }
 
     private void initializeStocksListView() {
+        stocksListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         stocksListView.setCellFactory(param -> new ListCell<>() {
             @Override
             protected void updateItem(Stock item, boolean empty) {
@@ -63,6 +136,54 @@ public class MainController {
                 }
             }
         });
+
+        stocksListView.setOnMouseClicked(event -> {
+            Stock stock = stocksListView.getSelectionModel().getSelectedItem();
+            chartStock(stock, DEFAULT_TIME_SERIES_TYPE);
+        });
+    }
+
+    private void chartStock(Stock stock, TimeSeriesType timeSeriesType) {
+        stockCurrentlyInChart = stock;
+        stockNameLabel.setText(stock.getName());
+        List<StockData> stockData = new ArrayList<>();
+        try {
+            stockData = stockScreenerRestClient.searchStockTimeSeries(stock.getSymbol(), timeSeriesType);
+        } catch (UserNotLoggedInException e) {
+            e.printStackTrace();
+        }
+        fillChart(stockData, timeSeriesType);
+        setDisableChartButtons(false);
+        setFavouriteButtonAccordingToUserFavourites(stock);
+    }
+
+    private void setFavouriteButtonAccordingToUserFavourites(Stock stock) {
+        try {
+            if (isFavourite(stock)) {
+                favourite.setText(REMOVE_FROM_FAVOURITES);
+            } else {
+                favourite.setText(ADD_TO_FAVOURITES);
+            }
+        } catch (UserNotLoggedInException e) {
+            e.printStackTrace();
+            favourite.setDisable(true);
+        }
+    }
+
+    private boolean isFavourite(Stock stock) throws UserNotLoggedInException {
+        return stockScreenerRestClient.getFavourites().stream().anyMatch(s -> s.getSymbol().equals(stock));
+    }
+
+    private void fillChart(List<StockData> stockData, TimeSeriesType timeSeriesType) {
+        clearChart();
+        xAxis.setLabel(timeSeriesType.toString());
+        XYChart.Series series = new XYChart.Series();
+        stockData.forEach(stockData1 -> series.getData().add(new XYChart.Data<String, Number>(stockData1.getDateTime().toLocalDate().toString(), stockData1.getClose())));
+        chart.getData().add(series);
+    }
+
+    private void clearChart() {
+        chart.getData().clear();
     }
 
     private void initializeSearchButton() {
